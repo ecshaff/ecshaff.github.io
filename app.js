237f2ae6e327d686fdaff95169e84b5ee80f1e27
ecshaff <emmyshaff@gmail.com>
2019-08-22 22:41:52 -0700

    updated form

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ $(document).ready(function() {
   $('.submit').click(function(){
     
     const email = $('.email').val()
-    const subject = $('.subject').val()
     const message = $('.message').val()
     const statusElm = $('.status')
     statusElm.empty()
     
     if(email.length > 5 && email.includes('@') && email.includes('.')) {
-      statusElm.append("<div>email is valid</div>")
     } else {
       event.preventDefault()
       statusElm.append("<div>email is not valid</div>")
     }
 
     if(subject.length > 0) {
-      statusElm.append("<div>subject is valid</div>")
     } else { 
       event.preventDefault()
       statusElm.append("<div>Subject field must be filled</div>")
     }
 
     if(message.length > 0) {
-      statusElm.append("<div>Message is valid</div>")
+      statusElm.append("<div>Sent!</div>")
     } else { 
       event.preventDefault()
       statusElm.append("<div>Message field must be filled</div>")
